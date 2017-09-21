35342c42f752072f322f726228cfb8c6d06f025d
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-09-21 11:10:16 +0800

    Fix CWD logic of run_squid script.

--- squid/run_squid.sh
+++ squid/run_squid.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CACHE=$(pwd)/.cache
-CONF=$(pwd)/squid.conf
+CWD="$(realpath $(dirname $0))"
+CACHE=${CWD}/.cache
+CONF=${CWD}/squid.conf
 
-cd $(dirname $0)
-mkdir $CACHE
+mkdir ${CACHE}
 docker run --name squid -d --restart=always \
   -p 8128:3128 \
-  -v $CONF:/etc/squid3/squid.conf \
-  -v $CACHE:/var/spool/squid3 \
+  -v ${CONF}:/etc/squid3/squid.conf \
+  -v ${CACHE}:/var/spool/squid3 \
   sameersbn/squid:3.3.8-23
 
